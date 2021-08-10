4ca6cf13a6385d152aea557dd7f261efd28dd921
marade <marade@users.noreply.github.com>
2021-08-10 11:17:09 -0700

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -6,4 +6,4 @@ sudo apt -y install python3-pip pigz samtools bcftools minimap2 picard-tools
 
 wget https://github.com/s4hts/HTStream/releases/download/v1.3.0-release/HTStream_v1.3.0-release.tar.gz && tar -xzvf HTStream_v1.3.0-release.tar.gz && sudo mv hts_* /usr/local/bin/ && rm HTStream_v1.3.0-release.tar.gz
 
-sudo -H pip install pandas biopython colorama pysam
+sudo -H pip install pandas biopython colorama pysam gffutils
