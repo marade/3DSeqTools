da88c417fae482c5738c7a6d6c50fe1169bbbfde
marade <marade@users.noreply.github.com>
2021-08-10 11:35:42 -0700

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -7,3 +7,5 @@ sudo apt -y install python-is-python3 python3-pip pigz samtools bcftools minimap
 wget https://github.com/s4hts/HTStream/releases/download/v1.3.0-release/HTStream_v1.3.0-release.tar.gz && tar -xzvf HTStream_v1.3.0-release.tar.gz && sudo mv hts_* /usr/local/bin/ && rm HTStream_v1.3.0-release.tar.gz
 
 sudo -H pip install pandas biopython colorama pysam gffutils
+
+echo "install.packages(\"evobiR\", repos=\"https://cran.rstudio.com\")" | sudo R --no-save
