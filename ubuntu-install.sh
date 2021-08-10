3f952cc2f59013fc1369a96c8d0b98459ae84eb9
marade <marade@users.noreply.github.com>
2021-08-10 11:24:39 -0700

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt -y update
 
-sudo apt -y install python3-pip pigz samtools bcftools minimap2 picard-tools
+sudo apt -y install python-is-python3 python3-pip pigz samtools bcftools minimap2 picard-tools
 
 wget https://github.com/s4hts/HTStream/releases/download/v1.3.0-release/HTStream_v1.3.0-release.tar.gz && tar -xzvf HTStream_v1.3.0-release.tar.gz && sudo mv hts_* /usr/local/bin/ && rm HTStream_v1.3.0-release.tar.gz
 
