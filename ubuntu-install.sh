276527480017a1082f497726ff98292e3dbb42fe
marade <marade@users.noreply.github.com>
2021-08-10 12:07:44 -0700

    Add files via upload

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -9,3 +9,4 @@ wget https://github.com/s4hts/HTStream/releases/download/v1.3.0-release/HTStream
 sudo -H pip install pandas biopython colorama pysam gffutils
 
 echo "install.packages(\"evobiR\", repos=\"https://cran.rstudio.com\")" | sudo R --no-save
+
