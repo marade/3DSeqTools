58acf3c740d89d0946119e4e24e57a9c4385937b
marade <marade@users.noreply.github.com>
2021-08-10 10:41:50 -0700

    Add files via upload

---
+++ ubuntu-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt -y update
+
+sudo apt -y install python3-pip pigz samtools minimap2 picard-tools
+
+wget https://github.com/s4hts/HTStream/releases/download/v1.3.0-release/HTStream_v1.3.0-release.tar.gz && tar -xzvf HTStream_v1.3.0-release.tar.gz && sudo mv hts_* /usr/local/bin/ && rm HTStream_v1.3.0-release.tar.gz
+
+sudo -H pip install pandas biopython colorama pysam
